62fa46d2db48acb877754c17f07f0679cebeb045
Walther <walcarher@gmail.com>
2021-04-15 15:46:46 +0200

    completed wnp validation

--- NonLinSysIdent.py
+++ NonLinSysIdent.py
@@ -456,123 +456,126 @@ def ThrAggModel(x ,b3 ,b2, b1, b0):
 
 
 #----------------------------------- k-Fold Cross Validation ------------------------------------
-# Number of folds
-k_folds = 10
-# Number of iterations
-iters = 50
-parameterDistLAT = []
-parameterDistE = []
-avLAT_NMRSE = 0
-avPOW_NMRSE = 0
-avE_NMRSE = 0
-avT_NMRSE = 0
-distNMRSE = []
-
-# Random seed used to debug 
-#np.random.seed(1234567890)
-for iter in range(iters):
-    # Dataset shuffle
-    shuffledData = np.array([WH, C, K, N, LAT, POW, E, T])
-    shuffledData = shuffledData[:, np.random.permutation(shuffledData.shape[1])]
-    # Dataset split in k-folds
-    foldSize = shuffledData.shape[1] / k_folds
-    for i in range(k_folds):
-        # Split for train data
-        trainData = np.delete(shuffledData, np.arange(i*foldSize,i*foldSize+foldSize,dtype=int), 1)
-        # Split for validation data
-        validationData = shuffledData[:,np.arange(i*foldSize,i*foldSize+foldSize,dtype=int)]
-        # Identification over training Dataset
-        LAT_parameters, LAT_covariance = curve_fit(LatAggModel, trainData[:4,:], trainData[4,:], maxfev=1000)
-        POW_parameters, POW_covariance = curve_fit(PowAggModel, trainData[:4,:], trainData[5,:], maxfev=1000)
-        E_parameters, E_covariance = curve_fit(EneAggModel, trainData[:4,:], trainData[6,:], maxfev=1000)
-        T_parameters, T_covariance = curve_fit(ThrAggModel, trainData[:4,:], trainData[7,:], maxfev=1000)
-        # Compute resulting NRMSE on validation Dataset fold
-        distNMRSE.append(NRMSE(validationData[4,:], validationData[:4,:], LatAggModel, LAT_parameters))
-        avLAT_NMRSE += NRMSE(validationData[4,:], validationData[:4,:], LatAggModel, LAT_parameters)
-        avPOW_NMRSE += NRMSE(trainData[5,:], trainData[:4,:], PowAggModel, POW_parameters)
-        avE_NMRSE += NRMSE(trainData[6,:], trainData[:4,:], EneAggModel, E_parameters)
-        avT_NMRSE += NRMSE(trainData[7,:], trainData[:4,:], ThrAggModel, T_parameters)
-        # Store obtained distribution per fold iteration
-        parameterDistLAT.append(np.concatenate((LAT_parameters[0]*selectedParameters[0], \
-                                LAT_parameters[1]*selectedParameters[1], \
-                                LAT_parameters[2]*selectedParameters[2], \
-                                LAT_parameters[3]*selectedParameters[3])))
-        parameterDistE.append(np.concatenate((E_parameters[0]*selectedParameters[8], \
-                                E_parameters[1]*selectedParameters[9], \
-                                E_parameters[2]*selectedParameters[10], \
-                                E_parameters[3]*selectedParameters[11])))
-# Average NRMSE metric
-k_folds = k_folds*iters
-avLAT_NMRSE = avLAT_NMRSE / k_folds
-avPOW_NMRSE = avPOW_NMRSE / k_folds
-avE_NMRSE = avE_NMRSE / k_folds
-avT_NMRSE = avT_NMRSE / k_folds
-
-#------------------------------------- Gaussian normal distribution hypothesis testing with KS ------------------------------
-distLATarray = np.array(parameterDistLAT)
-distEarray = np.array(parameterDistE)
-# Normalise
-distLATarray = (distLATarray - np.mean(distLATarray, axis=0)) / (np.amax(distLATarray, axis=0) - np.amin(distLATarray, axis=0))
-distEarray = (distEarray - np.mean(distEarray, axis=0))/ (np.amax(distEarray, axis=0) - np.amin(distEarray, axis=0))
-# Standarise
-#distLATarray = (distLATarray - np.mean(distLATarray, axis=0)) / np.std(distLATarray, axis=0)
-#distEarray = (distEarray - np.mean(distEarray, axis=0)) / np.std(distEarray, axis=0)
-
-# Distribution Test
-y = np.array(distNMRSE)
-plt.figure()
-plt.plot(y, label='NRMSE')
-plt.title('NRMSE per k-fold validation iteration')
-plt.grid()
-x = np.linspace(np.min(y),np.max(y),iters)
-bins = np.linspace(np.min(y),np.max(y),iters+1)
-hist, bin_edges = np.histogram(y, bins=bins, density=True)
-mean, var = norm.fit(y) # Get first 2 moments of data
-smean, svar, sk = skewnorm.fit(y) # Get first 3 moments of data
-#df, nc, cmean, cvar = ncx2.fit(y, 4) # Get 3 first moments of data
-gNorm = norm.pdf(x, mean, var) # Center and scale a Gaussian function
-sNorm = skewnorm.pdf(x, smean, svar, sk) # Center and scale a Skewed Gaussian function
-#chiSq = ncx2.pdf(x, 4, 1) # Center and scale a Chi-Square function
-plt.figure()
-plt.plot(x, gNorm,'r-', label='Norm PDF')
-plt.plot(x, sNorm,'g-', label='Skewed Norm PDF')
-#plt.plot(x, chiSq,'m-', label='Chi-Square PDF')
-plt.bar(bin_edges[:-1], hist, width = (max(bin_edges)-min(bin_edges))/iters)
-plt.title('NRMSE distribution')
-plt.xlim(np.min(x),np.max(x)) 
-plt.legend()
-# Print Normality Test results (Variable and p-value)
-#   Kolmogorov-Smirnov
-print(stats.kstest(hist, gNorm))
-print(stats.kstest(hist, sNorm))
-#   Shapiro-Wilk
-print(stats.shapiro(hist))
-#   Chi-Square
-#print(stats.chisquare(hist, gNorm))
-#print(stats.chisquare(hist, sNorm))
-# Q-Q plots
-#f, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2)
-f, (ax1,ax2) = plt.subplots(1,2)
-plt.title("Q-Q Plots")
-res = stats.probplot(y, dist=stats.norm(mean, var), plot=ax1)
-ax1.set_title("Normality Test (Non-Skewed)")
-resS = stats.probplot(y, dist=stats.skewnorm(smean, svar, sk), plot=ax2)
-ax2.set_title("Normality Test (Skewed)")
-#resX2 = stats.probplot(y, dist=stats.chi2(4), plot=ax3)
-#ax3.set_title("Chi-Square Test (k=4)")
-#resX2 = stats.probplot(y, dist=stats.chi2(10), plot=ax4)
-#ax4.set_title("Chi-Square Test (k=10)")
-# Auto-correlation analysys for White- or Gaussian-noise 
-y = (y - np.mean(y)) / (np.max(y) - np.min(y))
-corr = autocorr(y)
-plt.figure()
-plt.bar(np.linspace(0,k_folds-1,k_folds),corr[k_folds-1:])
-plt.title('NRMSE Auto-correlation')
-plt.grid()
-print('Average zero-element correlation value: %5.3f should be almost equal to variance: %5.3f' % (corr[k_folds-1]/k_folds, np.std(y)**2))
-print('Average of non-zero correlation values: %5.3f should be almost equal to 0' % np.mean(corr[k_folds:]))
-# Show parameter distribution after k-fold validation for box plotting    
 if args.validation_plot:
+    # Number of folds
+    k_folds = 10
+    # Number of iterations
+    iters = 50
+    parameterDistLAT = []
+    parameterDistE = []
+    avLAT_NMRSE = 0
+    avPOW_NMRSE = 0
+    avE_NMRSE = 0
+    avT_NMRSE = 0
+    distNMRSE = []
+
+    # Random seed used to debug 
+    #np.random.seed(1234567890)
+    for iter in range(iters):
+        # Dataset shuffle
+        shuffledData = np.array([WH, C, K, N, LAT, POW, E, T])
+        shuffledData = shuffledData[:, np.random.permutation(shuffledData.shape[1])]
+        # Dataset split in k-folds
+        foldSize = shuffledData.shape[1] / k_folds
+        for i in range(k_folds):
+            # Split for train data
+            trainData = np.delete(shuffledData, np.arange(i*foldSize,i*foldSize+foldSize,dtype=int), 1)
+            # Split for validation data
+            validationData = shuffledData[:,np.arange(i*foldSize,i*foldSize+foldSize,dtype=int)]
+            # Identification over training Dataset
+            LAT_parameters, LAT_covariance = curve_fit(LatAggModel, trainData[:4,:], trainData[4,:], maxfev=1000)
+            POW_parameters, POW_covariance = curve_fit(PowAggModel, trainData[:4,:], trainData[5,:], maxfev=1000)
+            E_parameters, E_covariance = curve_fit(EneAggModel, trainData[:4,:], trainData[6,:], maxfev=1000)
+            T_parameters, T_covariance = curve_fit(ThrAggModel, trainData[:4,:], trainData[7,:], maxfev=1000)
+            # Compute resulting NRMSE on validation Dataset fold
+            distNMRSE.append(NRMSE(validationData[4,:], validationData[:4,:], LatAggModel, LAT_parameters))
+            avLAT_NMRSE += NRMSE(validationData[4,:], validationData[:4,:], LatAggModel, LAT_parameters)
+            avPOW_NMRSE += NRMSE(trainData[5,:], trainData[:4,:], PowAggModel, POW_parameters)
+            avE_NMRSE += NRMSE(trainData[6,:], trainData[:4,:], EneAggModel, E_parameters)
+            avT_NMRSE += NRMSE(trainData[7,:], trainData[:4,:], ThrAggModel, T_parameters)
+            # Store obtained distribution per fold iteration
+            parameterDistLAT.append(np.concatenate((LAT_parameters[0]*selectedParameters[0], \
+                                    LAT_parameters[1]*selectedParameters[1], \
+                                    LAT_parameters[2]*selectedParameters[2], \
+                                    LAT_parameters[3]*selectedParameters[3])))
+            parameterDistE.append(np.concatenate((E_parameters[0]*selectedParameters[8], \
+                                    E_parameters[1]*selectedParameters[9], \
+                                    E_parameters[2]*selectedParameters[10], \
+                                    E_parameters[3]*selectedParameters[11])))
+    # Average NRMSE metric
+    k_folds = k_folds*iters
+    avLAT_NMRSE = avLAT_NMRSE / k_folds
+    avPOW_NMRSE = avPOW_NMRSE / k_folds
+    avE_NMRSE = avE_NMRSE / k_folds
+    avT_NMRSE = avT_NMRSE / k_folds
+
+    #------------------------------------- Gaussian normal distribution hypothesis testing with KS ------------------------------
+    distLATarray = np.array(parameterDistLAT)
+    distEarray = np.array(parameterDistE)
+    # Normalise
+    distLATarray = (distLATarray - np.mean(distLATarray, axis=0)) / (np.amax(distLATarray, axis=0) - np.amin(distLATarray, axis=0))
+    distEarray = (distEarray - np.mean(distEarray, axis=0))/ (np.amax(distEarray, axis=0) - np.amin(distEarray, axis=0))
+    # Standarise
+    #distLATarray = (distLATarray - np.mean(distLATarray, axis=0)) / np.std(distLATarray, axis=0)
+    #distEarray = (distEarray - np.mean(distEarray, axis=0)) / np.std(distEarray, axis=0)
+
+    # Distribution Test
+    y = np.array(distNMRSE)
+    y_centered = (y - np.mean(y)) / (np.max(y) - np.min(y))
+    y = (y-np.mean(y))/np.std(y)
+    plt.figure()
+    plt.plot(y, label='NRMSE')
+    plt.title('NRMSE per k-fold validation iteration')
+    plt.grid()
+    x = np.linspace(np.min(y),np.max(y),iters)
+    bins = np.linspace(np.min(y),np.max(y),iters+1)
+    hist, bin_edges = np.histogram(y, bins=bins, density=True)
+    mean, var = norm.fit(y) # Get first 2 moments of data
+    smean, svar, sk = skewnorm.fit(y) # Get first 3 moments of data
+    #df, nc, cmean, cvar = ncx2.fit(y, 4) # Get 3 first moments of data
+    gNorm = norm.pdf(x, mean, var) # Center and scale a Gaussian function
+    sNorm = skewnorm.pdf(x, smean, svar, sk) # Center and scale a Skewed Gaussian function
+    #chiSq = ncx2.pdf(x, 4, 1) # Center and scale a Chi-Square function
+    plt.figure()
+    plt.plot(x, gNorm,'r-', label='Norm PDF')
+    plt.plot(x, sNorm,'g-', label='Skewed Norm PDF')
+    #plt.plot(x, chiSq,'m-', label='Chi-Square PDF')
+    plt.bar(bin_edges[:-1], hist, width = (max(bin_edges)-min(bin_edges))/iters)
+    plt.title('NRMSE distribution')
+    plt.xlim(np.min(x),np.max(x)) 
+    plt.legend()
+    # Print Normality Test results (Variable and p-value)
+    #   Kolmogorov-Smirnov
+    print(stats.kstest(y, 'norm', args=(mean, var)))
+    print(stats.kstest(y, 'skewnorm', args=(smean, svar, sk)))
+    #   Shapiro-Wilk
+    print(stats.shapiro(y))
+    #   Chi-Square
+    #print(stats.chisquare(hist, gNorm))
+    #print(stats.chisquare(hist, sNorm))
+    # Q-Q plots
+    #f, ((ax1,ax2),(ax3,ax4)) = plt.subplots(2,2)
+    f, (ax1,ax2) = plt.subplots(1,2)
+    plt.title("Q-Q Plots")
+    res = stats.probplot(y, dist=stats.norm(mean, var), plot=ax1)
+    ax1.set_title("Normality Test (Non-Skewed)")
+    resS = stats.probplot(y, dist=stats.skewnorm(smean, svar, sk), plot=ax2)
+    ax2.set_title("Normality Test (Skewed)")
+    #resX2 = stats.probplot(y, dist=stats.chi2(4), plot=ax3)
+    #ax3.set_title("Chi-Square Test (k=4)")
+    #resX2 = stats.probplot(y, dist=stats.chi2(10), plot=ax4)
+    #ax4.set_title("Chi-Square Test (k=10)")
+    # Auto-correlation analysys for White- or Gaussian-noise 
+    y = y_centered
+    corr = autocorr(y)
+    plt.figure()
+    plt.bar(np.linspace(0,k_folds-1,k_folds),corr[k_folds-1:])
+    plt.title('NRMSE Auto-correlation')
+    plt.grid()
+    print('Average zero-element correlation value: %5.3f should be almost equal to variance: %5.3f' % (corr[k_folds-1]/k_folds, np.std(y)**2))
+    print('Average of non-zero correlation values: %5.3f should be almost equal to 0' % np.mean(corr[k_folds:]))
+    # Show parameter distribution after k-fold validation for box plotting    
+
     plt.figure()
     plt.violinplot(distLATarray,showmeans=False,showmedians=True,showextrema=True)
     plt.title('Parameter distribution for Latency model')
