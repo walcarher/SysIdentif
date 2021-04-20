76cdc10416dcbd0dfe97c1836d2f77507ffcf485
Walther <walcarher@gmail.com>
2021-04-20 15:45:04 +0200

    added CPU support

--- DataGenMultivariateCPU.py
+++ DataGenMultivariateCPU.py
@@ -27,6 +27,9 @@ E = 0
 T = 0
 dataset = []
 
+# Forcing CPU use. Check with 'nvpmodel -q' before proceeding to use 4-core ARM and/or 2-core Denver CPU 
+device = torch.device("cpu")
+
 # This files are to be found in the L4T version of TX2, this may vary in the future
 # It reads the latest CPU Power in mW from the INA3221 in the TegraTX2
 cpuLoadFile = '/sys/devices/3160000.i2c/i2c-0/0-0041/iio_device/in_power1_input'
@@ -42,13 +45,13 @@ for WH_in in WH_in_list:
                 def __init__(self):
                     super(Conv1x1_s1_Net, self).__init__()
                     # input channel, output channels, kernel size 1x1, stride 1-1, 0 padding
-                    self.conv1 = nn.Conv2d(C_in, C_out, 1, stride = 1, padding = 0).cuda()
+                    self.conv1 = nn.Conv2d(C_in, C_out, 1, stride = 1, padding = 0, device = device)
 
                 def forward(self, x):
                     # Maxpooling 2x2 and ReLu activation function
-                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2)).cuda()
-                    x = F.relu(self.conv1(x)).cuda()
-                    #x = self.conv1(x).cuda()
+                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+                    x = F.relu(self.conv1(x), device = device)
+                    #x = self.conv1(x)
                     return x
                         
             class Conv3x3_s1_Net(nn.Module):
@@ -56,13 +59,13 @@ for WH_in in WH_in_list:
                 def __init__(self):
                     super(Conv3x3_s1_Net, self).__init__()
                     # input channel, output channels, kernel size 3x3, stride 1-1, 0 padding
-                    self.conv1 = nn.Conv2d(C_in, C_out, 3, stride = 1, padding = 0).cuda()
+                    self.conv1 = nn.Conv2d(C_in, C_out, 3, stride = 1, padding = 0, device = device)
 
                 def forward(self, x):
                     # Maxpooling 2x2 and ReLu activation function
-                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2)).cuda()
-                    x = F.relu(self.conv1(x)).cuda()
-                    #x = self.conv1(x).cuda()
+                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+                    x = F.relu(self.conv1(x), device = device)
+                    #x = self.conv1(x)
                     return x
                         
             class Conv5x5_s1_Net(nn.Module):
@@ -70,13 +73,13 @@ for WH_in in WH_in_list:
                 def __init__(self):
                     super(Conv5x5_s1_Net, self).__init__()
                     # input channel, output channels, kernel size 7x7, stride 1-1, 0 padding
-                    self.conv1 = nn.Conv2d(C_in, C_out, 5, stride = 1, padding = 0).cuda()
+                    self.conv1 = nn.Conv2d(C_in, C_out, 5, stride = 1, padding = 0, device = device)
 
                 def forward(self, x):
                     # Maxpooling 2x2 and ReLu activation function
-                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2)).cuda()
-                    x = F.relu(self.conv1(x)).cuda()
-                    #x = self.conv1(x).cuda()
+                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+                    x = F.relu(self.conv1(x), device = device)
+                    #x = self.conv1(x)
                     return x
                         
             class Conv7x7_s1_Net(nn.Module):
@@ -84,13 +87,13 @@ for WH_in in WH_in_list:
                 def __init__(self):
                     super(Conv7x7_s1_Net, self).__init__()
                     # input channel, output channels, kernel size 7x7, stride 1-1, 0 padding
-                    self.conv1 = nn.Conv2d(C_in, C_out, 7, stride = 1, padding = 0).cuda()
+                    self.conv1 = nn.Conv2d(C_in, C_out, 7, stride = 1, padding = 0, device = device)
 
                 def forward(self, x):
                     # Maxpooling 2x2 and ReLu activation function
-                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2)).cuda()
-                    x = F.relu(self.conv1(x)).cuda()
-                    #x = self.conv1(x).cuda()
+                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+                    x = F.relu(self.conv1(x), device = device)
+                    #x = self.conv1(x)
                     return x
 
             class Conv11x11_s1_Net(nn.Module):
@@ -98,16 +101,16 @@ for WH_in in WH_in_list:
                 def __init__(self):
                     super(Conv11x11_s1_Net, self).__init__()
                     # input channel, output channels, kernel size 11x11, stride 1-1, 0 padding
-                    self.conv1 = nn.Conv2d(C_in, C_out, 11, stride = 1, padding = 0).cuda()
+                    self.conv1 = nn.Conv2d(C_in, C_out, 11, stride = 1, padding = 0, device = device)
 
                 def forward(self, x):
                     # Maxpooling 2x2 and ReLu activation function
-                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2)).cuda()
-                    x = F.relu(self.conv1(x)).cuda()
-                    #x = self.conv1(x).cuda()
+                    #x = F.max_pool2d(F.relu(self.conv1(x)),(2,2))
+                    x = F.relu(self.conv1(x), device = device)
+                    #x = self.conv1(x)
                     return x
 
-            convkxk_s1_net_list = [Conv1x1_s1_Net().cuda(), Conv3x3_s1_Net().cuda(), Conv5x5_s1_Net().cuda(), Conv7x7_s1_Net().cuda(), Conv11x11_s1_Net().cuda()]
+            convkxk_s1_net_list = [Conv1x1_s1_Net(), Conv3x3_s1_Net(), Conv5x5_s1_Net(), Conv7x7_s1_Net(), Conv11x11_s1_Net()]
             
             for convkxk_s1_net in convkxk_s1_net_list:
                       
@@ -117,26 +120,20 @@ for WH_in in WH_in_list:
                 elapsed_time = 0.0
                 iter = 0
                 power = 0
-                torch.cuda.seed()
-                input = torch.rand(1, C_in, WH_in, WH_in).cuda()
-                convkxk_s1_net.conv1.weight.data.random_().cuda()
-                convkxk_s1_net.conv1.bias.data.random_().cuda()
-                convkxk_s1_net.cuda()            
-                out = convkxk_s1_net(input).cuda()
-                torch.cuda.synchronize()                
+                input = torch.rand(1, C_in, WH_in, WH_in, device = device)
+                convkxk_s1_net.conv1.weight.data.random_()
+                convkxk_s1_net.conv1.bias.data.random_()          
+                out = convkxk_s1_net(input, device = device)              
                 print("Input Tensor Size: %d Number of Channels: %d Filter size: %d  Number of Filters: %d"  % (WH_in, C_in, convkxk_s1_net.conv1.kernel_size[0], C_out))
                 start = time.time()
                 # Iterate over multiple tests
                 while(iter < n_iter): 
-                    torch.cuda.seed()
-                    input = torch.rand(1, C_in, WH_in, WH_in).cuda()
-                    convkxk_s1_net.conv1.weight.data.random_().cuda()
-                    convkxk_s1_net.conv1.bias.data.random_().cuda()
-                    convkxk_s1_net.cuda()            
-                    out = convkxk_s1_net(input).cuda()
+                    input = torch.rand(1, C_in, WH_in, WH_in, device = device)
+                    convkxk_s1_net.conv1.weight.data.random_()
+                    convkxk_s1_net.conv1.bias.data.random_()           
+                    out = convkxk_s1_net(input, device = device)
                     with open(cpuLoadFile, 'r') as cpuFile:
                         power += float(cpuFile.read())
-                    torch.cuda.synchronize()
                     iter += 1
                  
                 elapsed_time = time.time() - start
@@ -148,7 +145,6 @@ for WH_in in WH_in_list:
                 POW = av_power/1000 # in Watts
                 E = POW * av_elapsed_time # in Joules
                 T = WH_in * WH_in * C_in * 4 / (av_elapsed_time * 1024 * 1024 * 1024) # in GBytes/seconds
-                torch.cuda.empty_cache()
                 
                 dataset.append([WH_in, C_in, convkxk_s1_net.conv1.kernel_size[0], C_out, LAT, POW, E, T])
 
