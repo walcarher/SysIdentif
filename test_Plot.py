c64536ee6938b3bf6bc40fb2d3e01bd55d389bfc
Walther <walcarher@gmail.com>
2021-05-27 15:29:10 +0200

    GPU RT test

---
+++ test_Plot.py
@@ -0,0 +1,27 @@
+import sys
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+from mpl_toolkits.mplot3d import Axes3D
+import pickle
+
+# Load GPU Power KPI time series to test
+file = open('PowerPlot_GPU.pkl', 'rb')
+if not file:
+    sys.exit("No PowerPlot_GPU.pkl file was found")
+else:
+    power = pickle.load(file)
+    powerEst = pickle.load(file)
+
+# Plot results
+plt.figure()
+x = np.linspace(1, len(power), len(power))
+y = np.asarray(power)
+y_norm = (y - np.min(y)) / (np.max(y)-np.min(y))
+plt.plot(x,y_norm)
+y = 1000*np.asarray(powerEst)
+y_norm = (y - np.min(y)) / (np.max(y)-np.min(y))
+plt.plot(x,y_norm)
+plt.grid()
+plt.show()
